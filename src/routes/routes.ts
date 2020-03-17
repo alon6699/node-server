278ea4e19474a823013a621bb4af28456ae87503
alon6699 <allonmaliii66@gmail.com>
2020-03-17 20:18:31 +0200

    handle updateTime route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const fs = require('fs');
+const path = './assets/content.txt';
 
 router.get('/content', (req, res) => {
-    fs.readFile('./assets/content.txt', 'utf8', (error, data) => {
+    fs.readFile(path, 'utf8', (error, data) => {
         if (!error) {
             res.send(data.toUpperCase());
         } else {
@@ -12,7 +13,12 @@ router.get('/content', (req, res) => {
 });
 
 router.get('/updateTime', (req, res) => {
-    res.send('updateTime');
+    res.send(readLastModifyTime());
 });
+
+const readLastModifyTime: () => Date = () => {
+    const stats = fs.statSync(path);
+    return stats.mtime as Date;
+}
 
 module.exports = router;
