68d8714c0c295fa37a60bed3d835e8bc5cf994f1
alon6699 <allonmaliii66@gmail.com>
2020-03-19 22:58:16 +0200

    refactor package.json send file with response.write and response.end in the end of the file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as fs from 'fs';
 import { promisify } from 'util';
 
-const router: express.Router = express.Router();
+export const router: express.Router = express.Router();
 const path: string = './assets/content.txt';
 const stat = promisify(fs.stat);
 
@@ -10,7 +10,11 @@ router.get('/content', (req: express.Request, res: express.Response) => {
     const readStream: fs.ReadStream = fs.createReadStream(path, { encoding: "utf8" });
 
     readStream.on('data', (data: string) => {
-        res.send(data.toUpperCase());
+        res.write(data.toUpperCase());
+    });
+
+    readStream.on('close', () => {
+        res.status(200).end('');
     });
 
     readStream.on('error', () => {
@@ -26,5 +30,3 @@ async function readLastModifyTime(): Promise<Date> {
     const stats: fs.StatsBase<any> = await stat(path);
     return stats.mtime;
 }
-
-export { router };
