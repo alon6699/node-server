6063e1b8c2d74a0dfb048bc928752ba8e8c71f60
alon6699 <allonmaliii66@gmail.com>
2020-03-19 23:01:08 +0200

    remove prefix for router

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { router } from "./routes/routes";
 const app: express.Application = express();
 const PORT: number = 3000;
 
-app.use('/', router);
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
